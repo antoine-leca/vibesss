490907061fbe94c170d26a99555512b804cd32e0
Thibaut Matian <thibaut.matian@gmail.com>
2026-04-30 13:36:22 +0200

    ajout du model notifs, preparation pour controller

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -34,6 +34,12 @@ const ItemManager = require("./ItemManager");
 models.item = new ItemManager();
 models.item.setDatabase(pool);
 
+
+const NotifManager = require("./NotifManager");
+
+models.notifs = new NotifManager();
+models.notifs.setDatabase(pool);
+
 // bonus: use a proxy to personalize error message,
 // when asking for a non existing model
 
