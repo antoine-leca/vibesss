ebba69c98797b47b3c6ed59259e01db719711dde
Veronique Marro <veroniquemarro@MacBook-Pro-de-Veronique.local>
2026-04-30 11:35:14 +0200

    Creation du model Article

---
+++ backend/src/models/ArticleManager.js
@@ -0,0 +1,43 @@
+const AbstractManager = require("./AbstractManager");
+
+class ArticleManager extends AbstractManager {
+    constructor() {
+        super({table: "aricles"});
+    }
+
+    insert(article) {
+    return this.database.query(
+        `INSERT INTO ${this.table} (user_id, blog_id, title, content_text, content_image, release_date, creation_date, status) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
+        [
+        article.user_id,
+        article.blog_id,
+        article.title,
+        article.content_text,
+        article.content_image,
+        article.release_date,
+        article.creation_date,
+        article.status
+        ]
+    );
+}
+
+    update(article) {
+    return this.database.query(
+    `UPDATE ${this.table} SET user_id = ?, blog_id = ?, title = ?, content_text = ?, content_image = ?, release_date = ?, status = ? WHERE id = ?`,
+    [
+        article.user_id,
+        article.blog_id,
+        article.title,
+        article.content_text,
+        article.content_image,
+        article.release_date,
+        article.status,
+        article.id
+
+    ]
+    );
+}
+}
+
+
+module.exports = ArticleManager;
--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -29,10 +29,11 @@ pool.getConnection().catch(() => {
 
 const models = {};
 
-const ItemManager = require("./ItemManager");
+const ArticleManager = require("./ArticleManager");
 
-models.item = new ItemManager();
-models.item.setDatabase(pool);
+models.article = new ArticleManager();
+models.article.setDatabase(pool);
+
 
 // bonus: use a proxy to personalize error message,
 // when asking for a non existing model
