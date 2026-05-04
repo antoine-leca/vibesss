73371a377f926c539be45c6acd7d1553469bc46f
Thibaut Matian <thibaut.matian@gmail.com>
2026-05-04 12:00:30 +0200

    oublie de l index.js

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -34,6 +34,9 @@ const ArticleManager = require("./ArticleManager");
 models.article = new ArticleManager();
 models.article.setDatabase(pool);
 
+const ReportManager = require("./ReportManager");
+models.article = new ArticleManager();
+models.article.setDatabase(pool);
 
 
 const NotifManager = require("./NotifManager");
