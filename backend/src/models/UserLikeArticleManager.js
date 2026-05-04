6d1ccfb416a105af03222481fd2d465f1d6a6322
Veronique Marro <veronique.marro@laplateforme.io>
2026-05-04 17:43:56 +0200

    retrait du IGNORE suite à l'ajout de la contrainte SQL

--- backend/src/models/UserLikeArticleManager.js
+++ backend/src/models/UserLikeArticleManager.js
@@ -1,24 +1,24 @@
-    const AbstractManager = require("./AbstractManager");
+        const AbstractManager = require("./AbstractManager");
 
-    class UserLikeArticleManager extends AbstractManager {
-        constructor() {
-            super({ table : "users_articles" });
-        }
+        class UserLikeArticleManager extends AbstractManager {
+            constructor() {
+                super({ table : "users_articles" });
+            }
 
 
-    insert(userId, articleId){
-            return this.database.query(`INSERT IGNORE INTO ${this.table} (user_id, article_id) VALUES (?,?)`,
-            [userId, articleId]
+        insert(userId, articleId){
+                return this.database.query(`INSERT INTO ${this.table} (user_id, article_id) VALUES (?,?)`,
+                [userId, articleId]
+                );
+            }
+
+        deleteLike(userId, articleId) {
+                return this.database.query(
+                `DELETE FROM ${this.table} WHERE user_id = ? AND article_id = ?`,
+                [userId, articleId]
             );
+            }
+            
         }
 
-    deleteLike(userId, articleId) {
-        return this.database.query(
-        `DELETE FROM ${this.table} WHERE user_id = ? AND article_id = ?`,
-        [userId, articleId]
-        );
-        }
-        
-    }
-
-    module.exports = UserLikeArticleManager;
+        module.exports = UserLikeArticleManager;
