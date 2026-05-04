3dbf85f31d674d1d871911e08191284b367878cc
Pharell Constance <pharell.constance@laplateforme.io>
2026-05-04 16:42:06 +0200

    fix syntax issue in commentManager

--- backend/src/models/CommentManager.js
+++ backend/src/models/CommentManager.js
@@ -2,7 +2,7 @@ const AbstractManager = require("./AbstractManager");
 
 class CommentManager extends AbstractManager {
   constructor() {
-    super({ table: "comment" });
+    super({ table: "comments" });
   }
 
   //Create
