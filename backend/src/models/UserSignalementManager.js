aec8b976380a19fcaab7914aa14ea4cc1e487b02
thibaut-matian <thibaut.matian@laplateforme.io>
2026-05-05 11:01:48 +0200

    Fix nullish coalescing for id_article assignment

--- backend/src/models/UserSignalementManager.js
+++ backend/src/models/UserSignalementManager.js
@@ -10,7 +10,7 @@ class UserSignalementManager extends AbstractManager {
       `INSERT INTO ${this.table} (id_user, id_article, id_blog, id_comment, id_signalement) VALUES (?, ?, ?, ?, ?)`,
       [
         signalement.id_user,
-        signalement.id_article ?? null, // Utilisation du nullish coalescing pour les champs optionnels
+        signalement.id_article ?? null, 
         signalement.id_blog ?? null,
         signalement.id_comment ?? null,
         signalement.id_signalement
