29f857e40aa20ab2cff0e74f63db2dc2af79050b
Veronique Marro <veronique.marro@laplateforme.io>
2026-05-05 14:01:47 +0200

    Création route des likes

--- backend/src/router.js
+++ backend/src/router.js
@@ -64,5 +64,11 @@ router.put("/blogs/:id", blogController.edit);
 router.delete("/blogs/:id", blogController.destroy);
 
 
+const userLikeArticleController = require("./controllers/userLikeArticleController");
+
+router.post("/user_articles", userLikeArticleController.add);
+router.delete("/user_articles", userLikeArticleController.destroy);
+
+
 
 module.exports = router;
