14767f34f85faf25e3bd46c9aaffd7dbac0aa1cd
Thibaut Matian <thibaut.matian@gmail.com>
2026-05-05 13:04:35 +0200

    correction de signalement a report pour plus d unicité dans le fichier UserSignalement qui devient UserReortManager

--- backend/src/models/UserSignalementManager.js
+++ backend/src/models/UserSignalementManager.js
@@ -1,22 +1,22 @@
 const AbstractManager = require("./AbstractManager");
 
-class UserSignalementManager extends AbstractManager {
+class UserReportManager extends AbstractManager {
   constructor() {
     super({ table: "users_reports" });
   }
 
-  insert(signalement) {
+  insert(report) {
     return this.database.query(
       `INSERT INTO ${this.table} (user_id, article_id, blog_id, comment_id, report_id) VALUES (?, ?, ?, ?, ?)`,
       [
-        signalement.user_id,
-        signalement.article_id,
-        signalement.blog_id,
-        signalement.comment_id,
-        signalement.report_id
+        report.user_id,
+        report.article_id,
+        report.blog_id,
+        report.comment_id,
+        report.report_id
       ]
     );
   }
 }
 
-module.exports = UserSignalementManager;
+module.exports = UserReportManager;
