e1a9634930bce01e8d75982db764a0ec4a1d28ce
Veronique Marro <veronique.marro@laplateforme.io>
2026-05-06 14:54:57 +0200

    Modifications verifyPassword

--- backend/src/auth.js
+++ backend/src/auth.js
@@ -1,21 +1,46 @@
-const argon2 = require("argon2");
+  const argon2 = require("argon2");
+  const jwt = require("jsonwebtoken");
 
-const hashingOptions = {
-  type: argon2.argon2id,
-  memoryCost: 2 ** 16,
-  timeCost: 5,
-  parallelism: 1,
-};
+  const hashingOptions = {
+    type: argon2.argon2id,
+    memoryCost: 2 ** 16,
+    timeCost: 5,
+    parallelism: 1,
+  };
 
-const hashPassword = async (plainPassword) => {
-  return argon2.hash(plainPassword, hashingOptions);
-};
+  const hashPassword = async (plainPassword) => {
+    return argon2.hash(plainPassword, hashingOptions);
+  };
 
-const verifyPassword = async (plainPassword, hashedPassword) => {
-  return argon2.verify(hashedPassword, plainPassword);
-};
 
-module.exports = {
-  hashPassword,
-  verifyPassword,
-};
+
+  const verifyPassword = async (req, res) => {
+
+  try {
+  const isVerified = await argon2.verify(req.user.hashedPassword, req.body.plainPassword);
+  
+  if (isVerified) {
+  const payload = {sub: req.user.id };
+
+  const token = jwt.sign(payload, process.env.JWT_SECRET, {
+    expiresIn: "4h",
+  });
+  delete req.user.hashedPassword;
+  res.send({token, user: req.user});
+
+  } else {
+    res.sendStatus(401);
+  }
+  }  catch (err) {
+          console.error(err);
+          res.sendStatus(500);
+  }
+  };
+
+
+
+
+  module.exports = {
+    hashPassword,
+    verifyPassword,
+  };
