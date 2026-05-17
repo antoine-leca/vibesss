0b87d910d71f91bbfafe182fb2795a56afc433ee
Veronique Marro <veronique.marro@laplateforme.io>
2026-05-17 16:00:24 +0200

    Création et intégration du footer

--- frontend/src/AppRouter.jsx
+++ frontend/src/AppRouter.jsx
@@ -2,18 +2,25 @@ import { useState, useEffect } from 'react';
 import { Navigate, Route, BrowserRouter as Router, Routes } from "react-router";
 
 import Home from './pages/Home';
-import Header from './components/layout/Header'; // 1. On importe ton Header 
+import Header from './components/layout/Header'; 
+import Footer from './components/layout/Footer'; 
 
 function AppRouter() {
   return (
     <Router>
-      {/* 2. On place le Header ici pour qu'il soit visible sur TOUTES les pages */}
-      <Header />
-      
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {/* Tu pourras ajouter tes autres routes ici (ex: /explore, /login...) */}
-      </Routes>
+      <div>
+
+        <Header />
+        <div className="flex-grow w-full">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </div>
+        
+        <Footer />
+        
+      </div>
     </Router>
   );
 }
